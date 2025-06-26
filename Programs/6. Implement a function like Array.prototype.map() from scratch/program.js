5d3b83f9b88b11b4b22536d644dff00596609396
Aashish Moktan <aashiislama@gmail.com>
2025-06-26 16:32:05 +0545

    implement custom Array.prototype.filter() function

--- Programs/6. Implement a function like Array.prototype.map() from scratch/program.js
+++ Programs/6. Implement a function like Array.prototype.map() from scratch/program.js
@@ -1,18 +1,17 @@
 Array.prototype.myMap = function (callback, thisArgs) {
-  const currentArr = this;
-
   const result = [];
+  const array = this;
 
-  for (let i = 0; i < currentArr.length; i++) {
-    if (i in currentArr) {
-      callback.call(thisArgs, currentArr[i], i, result);
+  for (let i = 0; i < array.length; i++) {
+    if (i in array) {
+      result.push(callback.call(thisArgs, array[i], i, array));
     }
   }
 
   return result;
 };
 
-const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 10].map((el, index) => {
+const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 10].myMap((el, index) => {
   return el + 1;
 });
 
